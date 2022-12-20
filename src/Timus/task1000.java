d336535d5e8130e26627b157fecb194ed1a96614
NechayAndrey <198019842005@mail.ru>
2022-12-20 11:25:40 +0500

    Initial commit

---
+++ src/Timus/task1000.java
@@ -0,0 +1,18 @@
+package Timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1000 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int result = a+b;
+
+        out.println(result);
+        out.flush();
+    }
+}
