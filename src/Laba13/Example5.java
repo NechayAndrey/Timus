86c8b682d2d7969867b8b949814c7e9015a05776
NechayAndrey <198019842005@mail.ru>
2023-04-24 21:46:40 +0500

    Initial commit

--- src/Laba13/Example5.java
+++ src/Laba13/Example5.java
@@ -1,18 +1,11 @@
 package laba13;
-
-/* Напишите функцию, которая находит максимальный элемент в массиве целых чисел при помощи многопоточности.
-Количество потоков должно быть равно количеству ядер процессора.*/
-
 import java.util.concurrent.*;
-
 public class Example5 {
-
     public static void main(String[] args) {
         int[] numbers = {10, 50, 30, 180, 210, 573, 40, 90};
         int numThreads = 8;
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         int[] highestValues = new int[numThreads];
-
         int partitionSize = numbers.length / numThreads;
         for (int i = 0; i < numThreads; i++) {
             int[] partition = new int[partitionSize];
@@ -20,14 +13,11 @@ public class Example5 {
             executor.execute(new MaxElementFinderThread(i, partition, highestValues));
         }
         executor.shutdown();
-
         try {
             executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < numThreads; i++) {
             if (highestValues[i] > max) {
@@ -37,18 +27,15 @@ public class Example5 {
         System.out.println("Наибольший элемент: " + max);
     }
 }
-
 class MaxElementFinderThread implements Runnable {
     private int threadIndex;
     private int[] numbers;
     private int[] highestValues;
-
     public MaxElementFinderThread(int threadIndex, int[] numbers, int[] highestValues) {
         this.threadIndex = threadIndex;
         this.numbers = numbers;
         this.highestValues = highestValues;
     }
-
     @Override
     public void run() {
         int max = Integer.MIN_VALUE;
@@ -59,5 +46,4 @@ class MaxElementFinderThread implements Runnable {
         }
         highestValues[threadIndex] = max;
     }
-
 }
